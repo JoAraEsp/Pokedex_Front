f4e5162c39f6bc93bfded0b5cf22149846c1c78a
Jorge Ara <213345@ids.upchiapas.edu.mx>
2022-11-04 00:53:22 -0600

    PokeDex_front

---
+++ PokeDex_front/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Body from "./Body";
+
+function App() {
+    return ( 
+        <>
+            <Header></Header>
+            <Body></Body>
+        </>
+     );
+}
+
+export default App;
