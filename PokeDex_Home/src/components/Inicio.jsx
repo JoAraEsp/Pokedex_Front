e5b095c78c9c3be6f20f9c25c91cfe6b6b3b4844
Jorge Ara <213345@ids.upchiapas.edu.mx>
2022-11-05 17:23:01 -0600

    cambios

---
+++ PokeDex_Home/src/components/Inicio.jsx
@@ -0,0 +1,50 @@
+import "../Styles/Inicio.css"
+import Pikachu from "../Imagenes/Pikachu.png"
+
+
+function Inicio() {
+    return ( 
+        <><div class="topic-container">
+            <h1>BIENVENIDO</h1>
+            <div class="body">
+                <div class="entrada">
+                    <p>Un saludo, en esta wiki encontrarás todo lo relacionado a Pokemon: información y entretenimiento. Esperamos tenga una
+                        buena estancia aquí. ¡Explore!
+                    </p>
+                </div>
+                <div class="content">
+                    <img className="Entraimg" src={Pikachu}></img>
+                </div>
+            </div>
+            <div class="body">
+                <div class="authors">
+                    <div class="username"><a href="">Usuario2</a></div>
+                    <div>Integrante de la comunidad</div>
+                    <img src={Usuario2} alt="" />
+                    <div>Posts: <u>38</u></div>
+                    <div>Puntos: <u>2830</u></div>
+                </div>
+                <div class="content">
+                    Se necesitan más moderadores.
+                    <br />no tengo problema en formar parte del equipo
+                    <br /><br />
+                    de moderadores.
+                    <br />
+                        Buenas y saludos a todos
+                        <br />
+                    </div>
+            </div>
+
+        </div><div class="comment-area hide" id="comment-area">
+                <textarea name="comment" id="" placeholder="comment here ... "></textarea>
+                <input type="submit" value="submit"></input>
+            </div><div class="comment-area hide" id="reply-area">
+                <textarea name="reply" id="" placeholder="reply here ... "></textarea>
+                <input type="submit" value="submit" />
+            </div>
+            </>
+
+     );
+}
+
+export default Inicio;
